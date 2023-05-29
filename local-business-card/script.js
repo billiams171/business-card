baf79f9a2e2db73a69adc640ff2c7301f0a2a19b
xxbrianwilliamsxx <xxbrianwilliamsxx@penguin>
2023-05-29 15:49:29 -0400

    I think it will work this time
    Perhaps it may work this time
    :

---
+++ local-business-card/script.js
@@ -0,0 +1,17 @@
+function showTime() {
+	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+}
+showTime();
+setInterval(function () {
+	showTime();
+}, 1000);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "0px";
+  }
+}
